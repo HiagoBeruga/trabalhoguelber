a7b214b8fd3e5ea3e404d9de77fe5bb3ad14e524
lucasleandro160 <alkhyz23@gmail.com>
2020-09-30 22:49:03 -0300

    Factory Pattern

--- trabalhoguelber/src/trabalhoguelber/Trabalhoguelber.java
+++ trabalhoguelber/src/trabalhoguelber/Trabalhoguelber.java
@@ -1,8 +1,15 @@
 package trabalhoguelber;
 
+
 public class Trabalhoguelber {
 
     public static void main(String[] args) {
+
+        FactoryAnimal factory = new FactoryAnimal();
+		String nome = "Cavalo";
+		boolean botaOvo = false;
+		factory.getAnimal(nome, botaOvo);
+	}
     }
     
 }
